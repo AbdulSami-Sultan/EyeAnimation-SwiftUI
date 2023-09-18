43cfa892828f69d121d885d32200b56c008fc0ad
Abdul Sami Sultan <56970013+AbdulSami-Sultan@users.noreply.github.com>
2023-09-18 21:09:04 +0500

    initial Commit
    initial Commit

---
+++ EyeAnimation-SwiftUI/EyeAnimation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeAnimation_SwiftUIApp.swift
+//  EyeAnimation-SwiftUI
+//
+//  Created by Abdul Sami Sultan on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct EyeAnimation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
